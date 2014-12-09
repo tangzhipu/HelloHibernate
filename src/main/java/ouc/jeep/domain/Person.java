ac55e7c073a20792593ba62bc110405f27de8901
tangzhipu <327021108@qq.com>
2014-12-09 15:09:10 +0800

    hibernate CRUD

---
+++ src/main/java/ouc/jeep/domain/Person.java
@@ -0,0 +1,34 @@
+package ouc.jeep.domain;
+
+import java.io.Serializable;
+
+/**
+ *
+ * Date-2014年12月9日
+ */
+public class Person implements Serializable{
+
+	private Long pid ;
+	private String pname ;
+	private String psex;
+	
+	public Long getPid() {
+		return pid;
+	}
+	public void setPid(Long pid) {
+		this.pid = pid;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public String getPsex() {
+		return psex;
+	}
+	public void setPsex(String psex) {
+		this.psex = psex;
+	}
+	
+}
---
+++ src/main/java/ouc/jeep/helloWorld/PersonTest.java
@@ -0,0 +1,90 @@
+package ouc.jeep.helloWorld;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.junit.Test;
+
+import ouc.jeep.domain.Person;
+import ouc.jeep.utils.HibernateUtil;
+
+/**
+ *
+ * Date-2014年12月9日
+ */
+public class PersonTest {
+
+	@Test
+	public void savaPerson(){
+		/*Configuration configuration = new Configuration();
+		configuration.configure();*/
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+		
+		Person person = new Person();
+		person.setPname("jeep");
+		person.setPsex("男");
+		session.save(person);
+		
+		transaction.commit();
+		session.close();
+	}
+	
+	@Test
+	public void getPerson(){
+		Configuration configuration = new Configuration();
+		configuration.configure();
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		
+		Person person = (Person) session.get(Person.class, 3L);
+		System.err.println(person.getPname()+" sex:"+person.getPsex()); 
+	}
+	
+	@Test
+	public void updatePerson(){
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		
+		
+		Transaction transaction = session.beginTransaction();
+		
+		Person person = (Person) session.get(Person.class, 3L);  //调用默认的构造函数
+		System.err.println(person.getPname()+" sex:"+person.getPsex()); 
+		person.setPname("zhipuTANG");
+		
+		System.err.println(person.getPname()+" xxx sex:"+person.getPsex()); 
+		session.update(person); 
+		
+		transaction.commit();
+		session.close();
+	}
+	
+	@Test
+	public void deletePerson(){
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		Transaction transcation = session.beginTransaction();
+		
+		Person person = (Person) session.get(Person.class, 3L);
+		session.delete(person); 
+		
+		transcation.commit();
+		session.close();
+		
+	}
+	
+	@Test
+	public void queryPerson(){
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		List<Person> list = session.createQuery("from Person").list();
+		System.err.println(list.size()); 
+	}
+	
+	
+}
