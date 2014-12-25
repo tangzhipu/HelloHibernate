296f956752ddf8fa7cc6e08c42c0ac00211c9967
tangzhipu <327021108@qq.com>
2014-12-25 09:53:05 +0800

    多对多的关系（学生、课程）

---
+++ src/main/java/ouc/jeep/associationMapping/manyToMany/Student.java
@@ -0,0 +1,43 @@
+package ouc.jeep.associationMapping.manyToMany;
+
+import java.io.Serializable;
+import java.util.Set;
+
+/**
+ * @description: 
+ * @author: jeep
+ * @date: 2014年12月24日
+ */
+public class Student implements Serializable{
+
+	private Long sid;
+	private String sname;
+	private String description;
+	private Set<Course> course;
+	
+	public Long getSid() {
+		return sid;
+	}
+	public void setSid(Long sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Set<Course> getCourse() {
+		return course;
+	}
+	public void setCourse(Set<Course> course) {
+		this.course = course;
+	}
+	
+}
