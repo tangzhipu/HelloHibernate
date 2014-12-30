abfa2ae00c4fe463b559eddd52702bc1e0c7ed78
tangzhipu <327021108@qq.com>
2014-12-30 09:10:44 +0800

    fetch策略

--- src/main/java/ouc/jeep/optimize/fetch/FetchTest.java
+++ src/main/java/ouc/jeep/optimize/fetch/FetchTest.java
@@ -8,6 +8,28 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.junit.Test;
 
+/**
+ * 抓取策略：
+   *  前提条件：必须是一个对象操作其关联对象
+      根据一的一方加载多的一方
+          <set fetch="join/select/subselect">
+          join 左外连接
+             一次性的把两张表的数据全部查询出来，只发出一条sql语句
+          select 默认
+             先加载classes,当得到students的时候，才要加载students
+          subselect  子查询
+                              通过需求分析判断，如果存在子查询，则选择该策略能提高效率
+      根据多的一方加载一的一方
+          这种情况不考虑，因为关联对象就一个数据，怎么样都可以
+抓取策略                                                                懒加载                                                                   效果
+  join                false/true/extra           只发出一条sql语句，懒加载不起作用
+  select              true/false/extra          发出n+1条sql语句，在遍历学生的时候才要加载学生/发出n+1条sql语句，但是在获取到学生的集合的时候发出/等同于true
+ subselect            true/false/extra          发出两条语句，其他的等同于上面的内容，懒加载和select一样
+
+针对的是集合
+	subselect==select+batch-size
+ */
+
 /**  关联对象
  * 抓取策略
  *    根据一个对象怎么样提取它的关联对象
@@ -73,6 +95,7 @@ public class FetchTest {
 	 *    从sql语句可以看出，join用的是左外连接
 	 *    用join该需求变成一条sql语句
 	 */
+	
 	@Test
 	public void testQueryClassesByCid(){
 		Session session = sessionFactory.openSession();
